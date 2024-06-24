dc141421ae5eb7116f6b4d80f0acbdf4170fb063
fire8327 <fire83274@gmail.com>
2024-06-25 01:04:52 +0300

    create hero, advantages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,43 @@
 <template>
-    <div>
-        123
+    <div class="flex max-lg:flex-col gap-6 items-center">
+        <div class="flex flex-col gap-6 w-full lg:w-1/2">
+            <p class="font-Roboto-Slab text-3xl font-semibold tracking-wider">Откройте Мир Настольных Приключений!</p>
+            <p class="opacity-70 tracking-widest">Погрузитесь в захватывающий мир стратегий, квестов и классических настольных игр. Идеально для вечеров в кругу семьи, дружеских встреч и увлекательных соревнований. Мы предлагаем игры, которые подойдут и начинающим игрокам, и опытным любителям.</p>
+        </div> 
+        <img src="/images/hero/index.jpg" alt="" class="w-full lg:w-1/2 rounded-xl">
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="font-Roboto-Slab text-3xl font-semibold tracking-wider">Почему выбирают нас?</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+            <div class="flex flex-col gap-4 rounded-xl p-4 border border-[#292929]/15 transition-all duration-500 hover:border-[#292929] hover:scale-105">
+                <div class="flex items-center gap-4">
+                    <p class="text-2xl font-Roboto-Slab">Широкий ассортимент</p>
+                    <Icon class="text-3xl shrink-0" name="game-icons:perspective-dice-six-faces-random"/>
+                </div>
+                <p class="opacity-70 tracking-widest">Мы предлагаем более 1000 различных игр на любой вкус: от стратегий и квестов до классических настолок и карточных игр.</p>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl p-4 border border-[#292929]/15 transition-all duration-500 hover:border-[#292929] hover:scale-105">
+                <div class="flex items-center gap-4">
+                    <p class="text-2xl font-Roboto-Slab">Уникальные новинки</p>
+                    <Icon class="text-3xl shrink-0" name="mdi:newspaper-variant-multiple"/>
+                </div>
+                <p class="opacity-70 tracking-widest">Будьте в курсе последних тенденций и первым приобретайте новейшие игры, которые только появились на рынке.</p>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl p-4 border border-[#292929]/15 transition-all duration-500 hover:border-[#292929] hover:scale-105">
+                <div class="flex items-center gap-4">
+                    <p class="text-2xl font-Roboto-Slab">Выгодные предложения</p>
+                    <Icon class="text-3xl shrink-0" name="material-symbols:award-star-rounded"/>
+                </div>
+                <p class="opacity-70 tracking-widest">Регулярные акции и скидки позволяют вам приобретать любимые игры по отличным ценам.</p>
+            </div>
+            <div class="flex flex-col gap-4 rounded-xl p-4 border border-[#292929]/15 transition-all duration-500 hover:border-[#292929] hover:scale-105">
+                <div class="flex items-center gap-4">
+                    <p class="text-2xl font-Roboto-Slab">Качество и оригинальность</p>
+                    <Icon class="text-3xl shrink-0" name="ph:lightbulb-filament-bold"/>
+                </div>
+                <p class="opacity-70 tracking-widest">Все игры в нашем магазине - только оригинальная продукция от проверенных производителей.</p>
+            </div>
+        </div>
     </div>
 </template>
 
