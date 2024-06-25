ba48105327287da31b1a837c4d8fefbf22ee2753
fire8327 <fire83274@gmail.com>
2024-06-25 08:50:32 +0300

    create footer

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="py-4 w-full border-t border-[#292929]">
+        <p class="text-center font-Roboto-SLab">© 2024 K3HA™. All Rights Reserved.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
