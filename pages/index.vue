588635d013eaaa7856f0a37bfc751eb3e569481a
fire8327 <fire83274@gmail.com>
2024-06-25 09:51:54 +0300

    add categorites

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,41 @@
             </div>
         </div>
     </div>
+    <div class="flex flex-col gap-6 w-full">
+        <p class="font-Roboto-Slab text-3xl font-semibold tracking-wider">Популярные категории</p>
+        <div class="flex max-lg:flex-col gap-6 items-center">
+            <div class="flex flex-col gap-6 w-full lg:w-1/2">
+                <p class="font-Roboto-Slab text-2xl font-semibold tracking-wider">Карточные игры</p>
+                <p class="opacity-70 tracking-widest">Компактные и захватывающие игры, которые легко взять с собой куда угодно. Прекрасный выбор для быстрых игровых сессий и веселого времяпрепровождения.</p>
+            </div> 
+            <img src="/images/hero/index.jpg" alt="" class="w-full lg:w-1/2 rounded-xl">
+        </div>
+        <div class="w-full h-px bg-[#292929]/50"></div>
+        <div class="flex max-lg:flex-col gap-6 items-center">
+            <div class="flex flex-col gap-6 w-full lg:w-1/2">
+                <p class="font-Roboto-Slab text-2xl font-semibold tracking-wider">Для всей семьи</p>
+                <p class="opacity-70 tracking-widest">Идеальные игры для семейного отдыха, где каждый найдет себе занятие по душе. Простые правила и интересный сюжет сделают вечер незабываемым.</p>
+            </div> 
+            <img src="/images/hero/index.jpg" alt="" class="w-full lg:w-1/2 rounded-xl lg:order-first">
+        </div>
+        <div class="w-full h-px bg-[#292929]/50"></div>
+        <div class="flex max-lg:flex-col gap-6 items-center">
+            <div class="flex flex-col gap-6 w-full lg:w-1/2">
+                <p class="font-Roboto-Slab text-2xl font-semibold tracking-wider">Стратегические игры</p>
+                <p class="opacity-70 tracking-widest">Испытайте свои аналитические способности и примите вызов сложных стратегий. Эти игры требуют продуманных ходов и долгосрочного планирования.</p>
+            </div> 
+            <img src="/images/hero/index.jpg" alt="" class="w-full lg:w-1/2 rounded-xl">
+        </div>
+        <div class="w-full h-px bg-[#292929]/50"></div>
+        <div class="flex max-lg:flex-col gap-6 items-center">
+            <div class="flex flex-col gap-6 w-full lg:w-1/2">
+                <p class="font-Roboto-Slab text-2xl font-semibold tracking-wider">Кооперативные игры</p>
+                <p class="opacity-70 tracking-widest">Работайте в команде, чтобы достичь общей цели и насладиться победой вместе. Идеально подходят для объединения усилий и совместного решения задач.</p>
+            </div> 
+            <img src="/images/hero/index.jpg" alt="" class="w-full lg:w-1/2 rounded-xl lg:order-first">
+        </div>
+        <NuxtLink class="px-4 py-2 rounded-xl border border-[#292929] self-end">В каталог</NuxtLink>
+    </div>
     <div class="flex flex-col gap-6">
         <p class="font-Roboto-Slab text-3xl font-semibold tracking-wider">Контакты</p>
         <OurContacts></OurContacts>
